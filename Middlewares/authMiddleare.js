f91698a0e88fd008f3b637e74c236b868be5d9bd
waqasra2022skipq <104453900+waqasra2022skipq@users.noreply.github.com>
2023-07-31 18:28:11 +0500

    VerifyToken middleware added

---
+++ Middlewares/authMiddleare.js
@@ -0,0 +1,29 @@
+const jwt = require('jsonwebtoken')
+let mongoUser=require('../Model/user')
+
+const verifyToken = async (req, res, next)=>{
+    let token;
+    if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+        try {
+            // get Token from headers
+            token = req.headers.authorization.split(' ')[1]
+            // Verify the token
+            const decoded = jwt.verify(token, process.env.TOKEN_KEY)
+            req.user_id = decoded.id
+            next();
+        } catch (error) {
+            return res.status(200).json({
+                success:false
+            })
+        }
+    }
+    if(!token) {
+        return res.status(200).json({
+            success:false
+        })
+    }
+}
+
+module.exports = {
+    verifyToken
+}
